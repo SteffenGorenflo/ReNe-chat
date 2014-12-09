4a44c2c079d7e6bad3b32acc8d6710cc475e3ba3
Steffen Gorenflo <stgorenf@htwg-konstanz.de>
2014-12-09 14:09:55 +0100

    python script for scanning ports

---
+++ chat1/scan_ports.py
@@ -0,0 +1,42 @@
+from errno import ECONNREFUSED
+from functools import partial
+from multiprocessing import Pool
+import socket
+
+NUM_CORES = 4
+
+def ping(host, port):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	
+	print("send to socket: " + str(port))
+	sock.settimeout(1)
+	sock.sendto("HELLO".encode("UTF-8"),(host,port))
+	
+	try:
+		data, addr = sock.recvfrom(250)
+		print("received: " + str(data))
+		return port
+	except socket.error as serr:
+		print("not reachable: " + str(port) + " because of " + str(serr))
+
+
+def scan_ports(host):
+	p = Pool(NUM_CORES)
+	ping_host = partial(ping, host)
+	return filter(bool, p.map(ping_host, range(1, 50)))
+
+
+def main(host=None):
+	if host is None:
+		host = "141.37.168.26"
+
+	print("\nScanning ports on " + host + " ...")
+	ports = list(scan_ports(host))
+	print("\nDone.")
+
+	print(str(len(ports)) + " ports available.")
+	print(ports)
+
+
+if __name__ == "__main__":
+	main()
